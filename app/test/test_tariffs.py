ad99af2d203a7059106fb2caaf1444cf47e5afad
Sokolovskaia <sk2m.sokolovskaya@gmail.com>
2019-04-20 14:45:48 +0300

    Template inheritance added

--- app/test/test_tariffs.py
+++ app/test/test_tariffs.py
@@ -1,5 +1,14 @@
 from app.tariffs import TariffManager, Tariff
 
 
-def test_actual():
-    pass
+class TestTariffManager:
+
+    def test_add(self):
+        tm = TariffManager()
+        expected = [{'name': 'Мой Онлайн', 'price': 0, 'price_period': 'month', 'gb': 0, 'gb_unlim': None, 'minutes': 0,
+                     'minutes_unlim_tele2': True, 'sms': 0, 'hit': False, 'archived': False}]
+        my_online = Tariff(name='Мой Онлайн', price=0, price_period='month', gb=0, gb_unlim=None, minutes=0,
+                           minutes_unlim_tele2=True, sms=0, hit=False, archived=False)
+        actual = tm.add(my_online)
+        assert expected == actual
+
