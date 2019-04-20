b4a7e127a2dd2f32815c06149f1bb84d62db2de6
Sokolovskaia <sk2m.sokolovskaya@gmail.com>
2019-04-20 16:10:59 +0300

    fixed #5

--- app/test/test_tariffs.py
+++ app/test/test_tariffs.py
@@ -1,14 +1,37 @@
 from app.tariffs import TariffManager, Tariff
 
 
-class TestTariffManager:
+def test_actual():
+    tm = TariffManager()
+    my_online_1 = Tariff('Мой Онлайн 1', price=0, price_period='month', gb=0, gb_unlim=None, minutes=0,
+                         minutes_unlim_tele2=True, sms=0, hit=False, archived=False)
+    my_online_2 = Tariff('Мой Онлайн 2', price=0, price_period='month', gb=0, gb_unlim=None, minutes=0,
+                         minutes_unlim_tele2=True, sms=0, hit=False, archived=False)
+    my_online_3 = Tariff('Мой Онлайн 3', price=0, price_period='month', gb=0, gb_unlim=None, minutes=0,
+                         minutes_unlim_tele2=True, sms=0, hit=False, archived=True)
+    my_online_4 = Tariff('Мой Онлайн 4', price=0, price_period='month', gb=0, gb_unlim=None, minutes=0,
+                         minutes_unlim_tele2=True, sms=0, hit=False, archived=True)
+    tm.add(my_online_1)
+    tm.add(my_online_2)
+    tm.add(my_online_3)
+    tm.add(my_online_4)
+    actual = tm.actual()
+    assert [my_online_1, my_online_2] == actual
 
-    def test_add(self):
-        tm = TariffManager()
-        expected = [{'name': 'Мой Онлайн', 'price': 0, 'price_period': 'month', 'gb': 0, 'gb_unlim': None, 'minutes': 0,
-                     'minutes_unlim_tele2': True, 'sms': 0, 'hit': False, 'archived': False}]
-        my_online = Tariff(name='Мой Онлайн', price=0, price_period='month', gb=0, gb_unlim=None, minutes=0,
-                           minutes_unlim_tele2=True, sms=0, hit=False, archived=False)
-        actual = tm.add(my_online)
-        assert expected == actual
 
+def test_archived():
+    tm = TariffManager()
+    my_online_1 = Tariff('Мой Онлайн 1', price=0, price_period='month', gb=0, gb_unlim=None, minutes=0,
+                         minutes_unlim_tele2=True, sms=0, hit=False, archived=False)
+    my_online_2 = Tariff('Мой Онлайн 2', price=0, price_period='month', gb=0, gb_unlim=None, minutes=0,
+                         minutes_unlim_tele2=True, sms=0, hit=False, archived=False)
+    my_online_3 = Tariff('Мой Онлайн 3', price=0, price_period='month', gb=0, gb_unlim=None, minutes=0,
+                         minutes_unlim_tele2=True, sms=0, hit=False, archived=True)
+    my_online_4 = Tariff('Мой Онлайн 4', price=0, price_period='month', gb=0, gb_unlim=None, minutes=0,
+                         minutes_unlim_tele2=True, sms=0, hit=False, archived=True)
+    tm.add(my_online_1)
+    tm.add(my_online_2)
+    tm.add(my_online_3)
+    tm.add(my_online_4)
+    archived = tm.archived()
+    assert [my_online_3, my_online_4] == archived
